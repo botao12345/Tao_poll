f6d13782c4f132dfd1439ce418717c940dc70b81
Tao Bo <botao12345@gmail.com>
2015-11-21 19:35:20 -0500

    Added view voter

--- app/controllers/voter_controller.rb
+++ app/controllers/voter_controller.rb
@@ -8,31 +8,30 @@ class VoterController < ApplicationController
     @links = Link.all
     @upvoter = User.find(params[:id])
 
+    @upvoter_name = @upvoter.name
+
     if Admin.first == nil
       admin = Admin.new
-      admin.who_voted.push(@upvoter)
+      admin.who_voted.push(@upvoter_name)
       admin.option = 1
     else
       admin = Admin.first
-      admin.who_voted.push(@upvoter)
+      admin.who_voted.push(@upvoter_name)
     end
 
     admin.save
 
 
-
     @link = Link.find(params[:link_id])
-    unless @link.vote.include?(@upvoter)
+    unless @link.vote.include?(@upvoter_name)
 
-      if admin.who_voted.include?(@upvoter)
+      if admin.who_voted.include?(@upvoter_name)
         @links.each do |link|
-          link.vote.delete(@upvoter)
+          link.vote.delete(@upvoter_name)
           link.save
         end
       end
-      @link.vote.push(@upvoter)
-
-
+      @link.vote.push(@upvoter_name)
     end
     @link.save
     # @links = Link.all
@@ -41,7 +40,6 @@ class VoterController < ApplicationController
     #   link.vote.clear
     #   link.save
     # end
-
 
 
   end
